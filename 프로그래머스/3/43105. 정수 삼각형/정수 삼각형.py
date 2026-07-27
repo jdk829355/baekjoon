ecdc6dc15682f0705006ad019a64f5bae2892b0e
jdk829355 <jdk829355@gmail.com>
2026-07-27 15:54:20 +0900

    [level 3] Title: 정수 삼각형 -BaekjoonHub

---
+++ 프로그래머스/3/43105. 정수 삼각형/정수 삼각형.py
@@ -0,0 +1,15 @@
+def solution(triangle):
+    n = len(triangle)
+    dp = [[0 for _ in range(i+1)] for i in range(n)]
+    dp[0][0] = triangle[0][0]
+    
+    for i in range(1, n):
+        for j in range(i+1):
+            if j < 1:
+                dp[i][j] = dp[i-1][j] + triangle[i][j]
+            elif j == i:
+                dp[i][j] = dp[i-1][j-1] + triangle[i][j]
+            else:
+                dp[i][j] = max(dp[i-1][j], dp[i-1][j-1]) + triangle[i][j]
+            
+    return max(dp[-1])
