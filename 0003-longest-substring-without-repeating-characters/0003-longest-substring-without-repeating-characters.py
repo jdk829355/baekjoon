93d6178cb16df5d3f61fdbe2ccd9225ec541310d
JUNG DAEGYUN <64572472+jdk829355@users.noreply.github.com>
2026-08-18 15:53:32 +0900

    Time: 163 ms (39.79%), Space: 20.2 MB (13.32%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
@@ -10,9 +10,10 @@ class Solution:
 
         for right in range(N):
             if s[right] in used and left <= used[s[right]]:
+                # 중복이 있는 경우 (문자가 사용 되었으며, 그게 left 뒤에 있는 경우)
                 left = used[s[right]] + 1
-                used[s[right]] = right
             else:
-                used[s[right]] = right
                 max_len = max(max_len, right - left + 1)
+            used[s[right]] = right
+            
         return max_len
