78d4e2658976c027e3a11cdf547a94bf6e71a846
JUNG DAEGYUN <64572472+jdk829355@users.noreply.github.com>
2026-08-18 15:51:14 +0900

    Time: 161 ms (39.9%), Space: 19.8 MB (27.3%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
@@ -1,42 +1,18 @@
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
-        if len(s) < 2:
-            return len(s)
+        N = len(s)
+        if N < 2:
+            return N
 
-        chars = set(c for c in s)
-        
-        def solve_n(n):
-            chars_map = {
-                c: 0 for c in chars
-            }
+        left = 0
+        used = {}
+        max_len = 0
 
-            for c in s[:n]:
-                chars_map[c] += 1
-        
-            if len(set(s[:n])) == n: return True
-
-            left, right = 0, n-1
-            
-            while right+1 < len(s):
-                chars_map[s[left]] -= 1
-                chars_map[s[right+1]] += 1
-
-                if chars_map[s[left]]==1 and len(set(s[left+1:right+2])) == n: return True
-
-                left += 1
-                right += 1
-            
-            return False
-        
-        st, en = 1, len(s)
-
-        while st < en:
-            mid = max((st+en)//2, (st+en+1)//2)
-            res = solve_n(mid)
-
-            if res:
-                st = mid
+        for right in range(N):
+            if s[right] in used and left <= used[s[right]]:
+                    left = used[s[right]] + 1
+                    used[s[right]] = right
             else:
-                en = mid-1
-        
-        return st
+                used[s[right]] = right
+                max_len = max(max_len, right - left + 1)
+        return max_len
