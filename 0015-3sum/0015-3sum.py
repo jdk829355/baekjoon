3f57af757678d431f025f0b123f061514ed34634
JUNG DAEGYUN <64572472+jdk829355@users.noreply.github.com>
2026-08-18 17:05:06 +0900

    Time: 655 ms (38.62%), Space: 22.1 MB (81.86%) - LeetHub

--- 0015-3sum/0015-3sum.py
+++ 0015-3sum/0015-3sum.py
@@ -12,21 +12,25 @@ class Solution:
 
             st = i+1
             en = N-1
-            visited = set()
 
             while st < en:
-                if tuple([nums[st], nums[en]]) in visited:
-                    st += 1
-                    en -= 1
-                    continue
 
                 s = nums[st] + nums[en] + nums[i]
                 
                 if s == 0:
                     res.add(tuple([nums[i], nums[st], nums[en]]))
-                    visited.add(tuple([nums[st], nums[en]]))
                     st += 1
                     en -= 1
+                    while st < en and st > 0 and en < N-1:
+                        if nums[st-1] == nums[st]: 
+                            st += 1 
+                        else: 
+                            break
+                        if nums[en+1] == nums[en]: 
+                            en -= 1 
+                        else: 
+                            break
+
                 elif s > 0:
                     en -= 1
                 else:
